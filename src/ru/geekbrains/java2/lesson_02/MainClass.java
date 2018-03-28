e106e8313763328a8c5c4e5f60d4c9f8ce422204
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-03-28 23:10:01 +1100

    классная работа

--- src/ru/geekbrains/java2/lesson_02/MainClass.java
+++ src/ru/geekbrains/java2/lesson_02/MainClass.java
@@ -1,4 +1,30 @@
 package ru.geekbrains.java2.lesson_02;
 
-public class Main {
+import javafx.scene.media.SubtitleTrack;
+
+public class MainClass {
+    public static void main(String[] args) {
+        a();
+    }
+
+    static void a() {
+        b();
+    }
+
+    static void b() {
+        c();
+    }
+
+    static void c() {
+        System.out.println("START");
+        try {
+            int x = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("AE");
+            return;
+        } finally {
+            System.out.println("FINALLY");
+        }
+        System.out.println("END");
+    }
 }
