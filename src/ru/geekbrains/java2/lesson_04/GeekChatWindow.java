ed3028ee60d88ae727398b9b3b5dee37f9cdecbe
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-04-06 13:29:15 +1100

    начинаю делать дз (с нуля не подглядывая в классную)

---
+++ src/ru/geekbrains/java2/lesson_04/GeekChatWindow.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.java2.lesson_04;
+
+import javax.swing.*;
+
+public class GeekChatWindow extends JFrame{
+    public GeekChatWindow() {
+        setBounds(750, 200, 400, 700);
+        
+    }
+}
